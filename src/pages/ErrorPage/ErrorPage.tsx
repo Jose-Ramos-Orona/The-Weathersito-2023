894594657b2c794af0a14cd37bfc7be26ccdcf15
Jose Ramos <ramosoronaj@gmail.com>
2023-01-25 11:53:16 +0100

    Add Error page

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = (): JSX.Element => {
+  return (
+    <h2>
+      I'm sorry. There seems to be a problem with the information you are asking
+      for. Try again please
+    </h2>
+  );
+};
+
+export default ErrorPage;
